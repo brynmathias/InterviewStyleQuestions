52023a3b6dc020f2b46893124bc0fb0bbf3a1976
Bryn Mathias <brynmathias@gmail.com>
2013-05-30 20:31:26 +0100

    some more things

--- recrusion.py
+++ recrusion.py
@@ -18,12 +18,18 @@ def zero_to_n(n):
   zero_to_n(n-1)
   print n
 
+def reverse_string(input):
+   if(len(input) < 1):return 0
+   print input[len(input)-1]
+   input = input[:len(input)-1]
+   reverse_string(input)
+
 
 
 def main():
    print Factorial(100)
    n_to_zero(100)
    zero_to_n(100)
-
+   reverse_string("the end")
 if __name__ == '__main__':
   main()
